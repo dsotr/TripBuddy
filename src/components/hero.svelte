234b7494bd0c69761b4ea89825ca7508e404938e
dsotr <dbiscr@gmail.com>
2025-05-26 15:14:33 +0100

    initial commit + hero

---
+++ src/components/hero.svelte
@@ -0,0 +1,42 @@
+<header>
+	<div class="container hero">
+		<h1>Lorem ipsum <span>dolor</span> sit.</h1>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+		<a href="/" class="btn">Start</a>
+	</div>
+</header>
+
+<style>
+	header {
+		width: 100vw;
+		height: 100vh;
+		background-color: var(--light-color);
+	}
+
+	.hero {
+		padding: 10rem 6rem;
+		display: flex;
+		flex-direction: column;
+		gap: 0;
+		/* align-items: left; */
+		/* justify-content: top; */
+	}
+
+	h1 {
+		color: var(--dark-color);
+		font-size: 5rem;
+		line-height: 1;
+
+		span {
+			color: var(--primary-color);
+		}
+	}
+
+	p {
+		font-size: 1.4rem;
+	}
+
+	.btn {
+		margin-top: 2rem;
+	}
+</style>
---
+++ src/components/navbar.svelte
@@ -0,0 +1,50 @@
+<nav>
+	<div class="container-lg navbar-flex">
+		<div>
+			<a href="/"><img class="logo" src="images/logo/logo.png" alt="koma" /></a>
+		</div>
+		<div class="social main-menu">
+			<a href="#">
+				<i class="fa-brands fa-facebook fa-xl"></i>
+			</a>
+			<a href="#">
+				<i class="fa-brands fa-instagram fa-xl"></i>
+			</a>
+			<a href="#">
+				<i class="fa-brands fa-twitter fa-xl"></i>
+			</a>
+		</div>
+		<div class="main-menu">
+			<ul>
+				<li><a href="#services">Servizi</a></li>
+				<li><a href="#prices">Prezzi</a></li>
+				<li><a href="#contacts">Contatti</a></li>
+				<li><a class="btn" href="#cta">Prenota</a></li>
+			</ul>
+		</div>
+		<div class="mobile-menu">
+			<div class="mobile-menu-toggle">
+				<i class="fa-solid fa-bars fa-xl"></i>
+			</div>
+			<div class="dyn-menu">
+				<ul>
+					<li><a href="#services">Servizi</a></li>
+					<li><a href="#prices">Prezzi</a></li>
+					<li><a href="#contacts">Contatti</a></li>
+					<li><a class="btn" href="#cta">Prenota</a></li>
+					<li class="social">
+						<a href="#">
+							<i class="fa-brands fa-facebook fa-xl"></i>
+						</a>
+						<a href="#">
+							<i class="fa-brands fa-instagram fa-xl"></i>
+						</a>
+						<a href="#">
+							<i class="fa-brands fa-twitter fa-xl"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</nav>
