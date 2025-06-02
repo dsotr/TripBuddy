d2afe19e0d3ff6fb8c2f234eab98869a3a51de4c
Daniele <d.conca@gmail.com>
2025-06-02 18:44:47 +0200

    scss for media query variables. Mobile Hero component

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -6,37 +6,77 @@
 	</div>
 </header>
 
-<style>
+<style lang="scss">
 	header {
 		width: 100vw;
 		height: 100vh;
 		background-color: var(--light-color);
-	}
 
-	.hero {
-		padding: 10rem 6rem;
-		display: flex;
-		flex-direction: column;
-		gap: 0;
-		/* align-items: left; */
-		/* justify-content: top; */
-	}
+		.hero {
+			padding: 10rem 6rem;
+			display: flex;
+			flex-direction: column;
+			gap: 0;
+			/* align-items: left; */
+			/* justify-content: top; */
+		}
 
-	h1 {
-		color: var(--dark-color);
-		font-size: 5rem;
-		line-height: 1;
+		h1 {
+			color: var(--dark-color);
+			font-size: 5rem;
+			line-height: 1;
 
-		span {
-			color: var(--primary-color);
+			span {
+				color: var(--primary-color);
+			}
+		}
+
+		p {
+			font-size: 1.4rem;
+		}
+
+		.btn {
+			margin-top: 2rem;
 		}
 	}
 
-	p {
-		font-size: 1.4rem;
-	}
+	@media (max-width: $phone-width) {
+		header {
+			/* background: no-repeat url("../images/background-desktop.jpg"), #333; */
+			background:
+				no-repeat url('../images/background-mobile.jpg'),
+				var(--light-color);
+			background-size: cover;
+			background-position: bottom center;
 
-	.btn {
-		margin-top: 2rem;
+			.hero {
+				flex-direction: column;
+				text-align: center;
+			}
+
+			.hero-image {
+				margin: 20% auto 0;
+				align-self: center;
+			}
+
+			h1 {
+				font-size: 3rem;
+				margin-bottom: 3rem;
+			}
+
+			p {
+				font-size: 1rem;
+				letter-spacing: 0rem;
+				margin-bottom: 3rem;
+			}
+
+			img {
+				width: 150px;
+			}
+
+			.btn {
+				margin: 0 auto;
+			}
+		}
 	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
+import { sveltePreprocess } from 'svelte-preprocess';
 
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: { adapter: adapter() },
+	preprocess: sveltePreprocess({
+		scss: {
+			// Allow scss variables to be used in every component.
+			// Not using namespaces, actually using * as I won-t run into conflicts, otherwise:
+			// https://sass-lang.com/documentation/at-rules/use/#loading-members
+			prependData: `@use './src/variables.scss' as *;`
+		}
+	})
+};
 
 export default config;
