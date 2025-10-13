d3a503fa47460f1533d8153b8ca635c0b42645da
Daniele <d.conca@gmail.com>
2025-10-13 21:42:41 +0200

    TripsList component

---
+++ src/components/tripList.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Trip from '../components/trip.svelte';
+
+	let { trips } = $props();
+</script>
+
+<ul>
+	{#each trips as trip}
+		<Trip {trip} />
+		<!-- <li>{trip.name} - {trip.destination} - {trip.start_date} - {trip.end_date}</li> -->
+	{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,12 @@
 <script>
 	import Hero from '../components/hero.svelte';
 	import Trip from '../components/trip.svelte';
+	import TripList from '../components/tripList.svelte';
 	let { data } = $props();
 	console.log(data);
 </script>
 
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
-
-<Hero />
-
-<h1>Documents from Supabase</h1>
-
-<ul>
-	{#each data.trips as trip}
-		<Trip {trip} />
-		<!-- <li>{trip.name} - {trip.destination} - {trip.start_date} - {trip.end_date}</li> -->
-	{/each}
-</ul>
+<TripList trips={data.trips} />
 
 <p>{data.docs.length} documents found</p>
 <ul>
