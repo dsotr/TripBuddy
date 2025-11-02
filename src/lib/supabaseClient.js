38296aad0a1c477be692725e946764e07db8739c
dconca <dconca@google.com>
2025-11-02 21:10:54 +0100

    server hooks

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabaseSecretKey = import.meta.env.SUPABASE_SECRET_KEY;
+// const supabaseSecretKey = import.meta.env.SUPABASE_SECRET_KEY;
 
 // export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export const supabase = createClient(supabaseUrl, supabaseSecretKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
